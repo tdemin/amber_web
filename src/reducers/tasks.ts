4175e2883c40459310059631a40d15d153c3ffeb
Timur Demin <me@tdem.in>
2019-10-02 21:57:38 +0500

    Fix weird behaviour on task deletion

--- src/reducers/tasks.ts
+++ src/reducers/tasks.ts
@@ -4,43 +4,42 @@ import { Task } from "../typings/tasks";
 
 export interface TaskState {
     tasks: Task[];
-    /** Hack. Needed because react-redux won't fire updates unless we change
-     * object references, which is not the case for arrays. */
-    lastMod: number;
 }
 
 export const taskReducer = (
-    state: TaskState = { tasks: [], lastMod: Date.now() },
+    state: TaskState = { tasks: [] },
     action: TaskAction
 ): TaskState => {
     switch (action.type) {
         case Actions.TasksFetch:
-            return { tasks: action.data as Task[], lastMod: Date.now() };
+            return { tasks: action.data as Task[] };
         case Actions.TasksFetchError:
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         case Actions.TaskCreate: {
             state.tasks.push(action.data as Task);
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         }
         case Actions.TaskCreateError: {
             const newTask = action.data as Task;
             newTask.ToSync = true;
             state.tasks.push(newTask);
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         }
         case Actions.TaskDelete: {
-            const index = state.tasks.findIndex(
-                (task) => task.ID === (action.data as Task).ID
-            );
-            state.tasks.splice(index);
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return {
+                tasks: [
+                    ...state.tasks.filter(
+                        (task) => task.ID !== (action.data as Task).ID
+                    ),
+                ],
+            };
         }
         case Actions.TaskDeleteError: {
             const index = state.tasks.findIndex(
                 (task) => task.ID === (action.data as Task).ID
             );
             state.tasks[index].ToRemove = true;
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         }
         case Actions.TaskUpdate: {
             const index = state.tasks.findIndex(
@@ -48,7 +47,7 @@ export const taskReducer = (
             );
             state.tasks[index] = action.data as Task;
             state.tasks[index].LastMod = Date.now();
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         }
         case Actions.TaskUpdateError: {
             const index = state.tasks.findIndex(
@@ -57,7 +56,7 @@ export const taskReducer = (
             state.tasks[index] = action.data as Task;
             state.tasks[index].ToSync = true;
             state.tasks[index].LastMod = Date.now();
-            return { tasks: [...state.tasks], lastMod: Date.now() };
+            return { tasks: [...state.tasks] };
         }
         default:
             return state;
