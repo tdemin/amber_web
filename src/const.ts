84815a4ebfe1e34d7d6a7d5557572733c798a95d
Timur Demin <me@tdem.in>
2019-10-12 13:43:59 +0500

    Allow for a network timeout of 5s (fixes #16)

--- src/const.ts
+++ src/const.ts
@@ -5,3 +5,6 @@ export const appFullName: string = "Amber Web";
 export const appName: string = "amber_web";
 export const appAuthor: string = "Timur Demin";
 export const appHomePage: string = "https://git.tdem.in/tdemin/amber_web";
+
+/** Network timeout (in ms) as used by Axios. */
+export const networkTimeout: number = 5000;
