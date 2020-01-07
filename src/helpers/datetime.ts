9f31a7946327373fa1b2dee14e4f6278a61b9838
Timur Demin <me@tdem.in>
2020-01-07 21:37:01 +0500

    Remove unneeded division from the epoch time helper

--- src/helpers/datetime.ts
+++ src/helpers/datetime.ts
@@ -5,5 +5,4 @@ export const TimeFormat = "HH:mm";
  * Converts a JS date to epoch timestamp.
  * @param date Date in JS `Date` class
  */
-export const dateTimeToUnixTime = (date: Date) =>
-    parseInt((date.getTime() / 1000).toFixed(0));
+export const dateTimeToUnixTime = (date: Date) => date.getTime();
