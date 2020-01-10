44a07a5712b0ca76d487c0b8d827f6a5f0099a9a
Timur Demin <me@tdem.in>
2020-01-10 19:05:47 +0500

    Make tasks that have reminders associated with them look better on the main screen

--- src/views/components/taskLine.tsx
+++ src/views/components/taskLine.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter, RouteComponentProps as RCP } from "react-router";
 import format from "date-fns/format";
 
 import Level from "../components/bulma/level";
 import Button from "../components/bulma/button";
-import Link from "../components/link";
 
 import { Task } from "../../typings/tasks";
+import { Dispatch } from "../../typings/react";
 
 import { updateTask } from "../../actions/tasks";
 
 import strings from "../assets/locales";
-import { Dispatch } from "../../typings/react";
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
     update: (task: Task) => dispatch(updateTask(task)),
 });
 
-interface Props extends ReturnType<typeof mapDispatchToProps> {
+interface Props extends ReturnType<typeof mapDispatchToProps>, RCP {
     level: number;
     task: Task;
 }
@@ -43,6 +43,7 @@ class TaskLine extends React.Component<Props, State> {
     // Pp expands to a localized date string that looks like this:
     // "05/29/1453, 12:00 AM"
     fmtDate = (date: number) => format(new Date(date), "Pp");
+    gotoEditor = () => this.props.history.push(`/task/${this.state.task.ID}`);
     render = () => {
         const {
             ID,
@@ -59,19 +60,31 @@ class TaskLine extends React.Component<Props, State> {
         Reminder && classNames.push("taskHasReminder");
         return (
             <Level level isMobile className={classNames.join(" ")}>
-                <Level levelItem levelLeft className="taskText">
-                    <Link to={`/task/${ID}`}>
-                        <span className="taskID">{`#${ID} - `}</span>
+                <Level
+                    level
+                    levelItem
+                    levelLeft
+                    className="taskData"
+                    onClick={this.gotoEditor}
+                >
+                    <Level levelItem className="taskTextWrapper">
+                        <span className="taskID">{`#${ID}`}</span>
+                        {/* eslint-disable react/jsx-no-literals */}
+                        &nbsp;
                         <span className="taskText">{Text}</span>
+                    </Level>
+                    <Level levelItem className="taskDates">
                         <span className="taskDeadline">
-                            {` - (D: ${this.fmtDate(Deadline)})`}
+                            {`(D: ${this.fmtDate(Deadline)})`}
+                            {/* eslint-disable react/jsx-no-literals */}
+                            &nbsp;
                         </span>
                         <span className="taskReminder">
-                            {` - (R: ${this.fmtDate(Reminder)})`}
+                            {`(R: ${this.fmtDate(Reminder)})`}
                         </span>
-                    </Link>
+                    </Level>
                 </Level>
-                <Level levelItem levelRight>
+                <Level levelItem levelRight className="taskButton">
                     <Button
                         value={
                             Completed
@@ -86,4 +99,4 @@ class TaskLine extends React.Component<Props, State> {
     };
 }
 
-export default connect(null, mapDispatchToProps)(TaskLine);
+export default withRouter(connect(null, mapDispatchToProps)(TaskLine));
